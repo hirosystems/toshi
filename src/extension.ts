8b628a41a4695edc7cf49393e2e432ae5cc51349
Brice <brice@hiro.so>
2022-07-28 01:29:23 -0400

    refactor: refactor run command (#3)
    * Started handling of next/prev lesson, but it doesn't seem to work.
    * Modified run command to act on active file

--- src/extension.ts
+++ src/extension.ts
@@ -14,13 +14,16 @@ const initWasm = init();
 const { workspaceFolders, fs } = workspace;
 const { uri: workspaceUri } = workspaceFolders![0];
 
+const states = ["lesson1", "mission1"];
+let currentState = 0;
+
 export async function activate(context: vscode.ExtensionContext) {
   console.log('"toshi-extension" is now active');
   vscode.commands.executeCommand("toshi-extension.gameView.focus");
   vscode.commands.executeCommand("workbench.action.positionPanelRight");
   // vscode.commands.executeCommand("workbench.action.maximizeEditor");
 
-  const gameFile: vscode.Uri = Uri.joinPath(workspaceUri, "game.clar");
+  const gameFile: vscode.Uri = Uri.joinPath(workspaceUri, `${states[0]}.clar`);
   vscode.workspace.openTextDocument(gameFile);
 
   const gameViewProvider = new GameViewProvider(context.extensionUri);
@@ -76,61 +79,54 @@ export async function activate(context: vscode.ExtensionContext) {
   );
   context.subscriptions.push(testInstructions);
 
-  const ACTION_PREFIX = '"@ACTION@: ';
-
   const runCode = vscode.commands.registerCommand(
     "toshi-extension.runCode",
     async () => {
-      // Read the user's input file
-      const userCode = fileArrayToString(await fs.readFile(gameFile));
-
-      const result = interpret(`${mission1}\n${userCode}`);
-      const instructions: Instructions = [];
-
-      for (const event of result.events) {
-        if (event.type !== "contract_event") continue;
-        if (event.contract_event.topic !== "print") continue;
-        const printed = event.contract_event.value;
-        if (printed.startsWith(ACTION_PREFIX)) {
-          const action = printed.substring(
-            ACTION_PREFIX.length,
-            printed.length - 1,
-          );
-          const args = action.split(" ");
-          instructions.push({
-            target: "game",
-            type: "action",
-            args,
-          });
-        } else {
-          instructions.push({
-            target: "console",
-            type: "print",
-            text: printed,
-          });
-        }
-      }
-
-      for (const diagnostic of result.diagnostics) {
-        instructions.push({
-          target: "console",
-          type: "error",
-          text: diagnostic,
-        });
-      }
-
-      if (result.result) {
-        instructions.push({
-          target: "console",
-          type: "result",
-          text: result.result,
-        });
-      }
-
-      gameViewProvider.sendInstructions(instructions);
+      await run(gameViewProvider);
     },
   );
   context.subscriptions.push(runCode);
+
+  const nextLesson = vscode.commands.registerCommand(
+    "toshi-extension.nextLesson",
+    () => {
+      if (currentState === states.length - 1) {
+        window.showErrorMessage("No more lessons.");
+        return;
+      }
+
+      currentState += 1;
+      const current = states[currentState];
+
+      const gameFile: vscode.Uri = Uri.joinPath(
+        workspaceUri,
+        `${current}.clar`,
+      );
+      window.showInformationMessage(`opening: ${gameFile}`);
+      vscode.workspace.openTextDocument(gameFile);
+    },
+  );
+  context.subscriptions.push(nextLesson);
+
+  const prevLesson = vscode.commands.registerCommand(
+    "toshi-extension.prevLesson",
+    () => {
+      if (currentState === 0) {
+        window.showErrorMessage("No more lessons.");
+        return;
+      }
+
+      currentState -= 1;
+      const current = states[currentState];
+
+      const gameFile: vscode.Uri = Uri.joinPath(
+        workspaceUri,
+        `${current}.clar`,
+      );
+      vscode.workspace.openTextDocument(gameFile);
+    },
+  );
+  context.subscriptions.push(prevLesson);
 
   const startCommand = vscode.commands.registerCommand(
     "toshi-extension.launchToshi",
@@ -150,4 +146,65 @@ function fileArrayToString(bufferArray: Uint8Array) {
   return Array.from(bufferArray)
     .map((item) => String.fromCharCode(item))
     .join("");
+}
+
+async function run(gameViewProvider: GameViewProvider) {
+  const activeFile = vscode.window.activeTextEditor?.document.uri;
+  if (!activeFile) {
+    window.showErrorMessage("No active file.");
+    return;
+  }
+
+  // Read the user's code
+  const userCode = fileArrayToString(await fs.readFile(activeFile));
+
+  // Read the hidden code
+  const hiddenCode = require(`./clarity/${activeFile.fsPath.split("/").pop()}`);
+
+  const result = interpret(`${hiddenCode}\n${userCode}`);
+  const instructions: Instructions = [];
+
+  const ACTION_PREFIX = '"@ACTION@: ';
+
+  for (const event of result.events) {
+    if (event.type !== "contract_event") continue;
+    if (event.contract_event.topic !== "print") continue;
+    const printed = event.contract_event.value;
+    if (printed.startsWith(ACTION_PREFIX)) {
+      const action = printed.substring(
+        ACTION_PREFIX.length,
+        printed.length - 1,
+      );
+      const args = action.split(" ");
+      instructions.push({
+        target: "game",
+        type: "action",
+        args,
+      });
+    } else {
+      instructions.push({
+        target: "console",
+        type: "print",
+        text: printed,
+      });
+    }
+  }
+
+  for (const diagnostic of result.diagnostics) {
+    instructions.push({
+      target: "console",
+      type: "error",
+      text: diagnostic,
+    });
+  }
+
+  if (result.result) {
+    instructions.push({
+      target: "console",
+      type: "result",
+      text: result.result,
+    });
+  }
+
+  gameViewProvider.sendInstructions(instructions);
 }
